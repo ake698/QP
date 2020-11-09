288251cce4ca69715233d8f0a5a359ea0ef4843c
hunk5431@gmail.com <hunk5431@gmail.com>
2020-11-09 22:15:56 +0800

    添加项目文件。

---
+++ QP/Controllers/QPController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QP.Models;
+
+namespace QP.Controllers
+{
+
+    public class QPController : Controller
+    {
+        private readonly ILogger<QPController> _logger;
+
+        public QPController(ILogger<QPController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("detail")]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        [Route("play")]
+        public IActionResult Play()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
