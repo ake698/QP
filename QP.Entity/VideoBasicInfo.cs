560d2dbe6e341cb9baf594e02f97af043f1e4a55
shiqing.feng <shiqing.feng@starlight-sms.com>
2020-11-12 16:55:10 +0800

    Update basic info

--- QP.Entity/VideoBasicInfo.cs
+++ QP.Entity/VideoBasicInfo.cs
@@ -16,6 +16,6 @@
         public string Remark { get; set; }
         public string Country { get; set; }
         public string Language { get; set; }
-        public int Year { get; set; }
+        public string Year { get; set; }
     }
 }
